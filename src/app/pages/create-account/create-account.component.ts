012370d9ca69d63f77d8f890537256d3679ad011
jstewart3800 <56611713+jstewart3800@users.noreply.github.com>
2020-02-06 14:11:23 -0800

    continued building  account creation functions

--- src/app/pages/create-account/create-account.component.ts
+++ src/app/pages/create-account/create-account.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from 'src/app/services/user.service';
+import { User } from 'src/app/interfaces/user';
 
 @Component({
   selector: 'app-create-account',
@@ -7,11 +9,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateAccountComponent implements OnInit {
 
-  constructor() { }
+  constructor(private uService: UserService) { }
 
   ngOnInit() {
   }
+  //fn.value,ln.value,ea.value,pw.value,cpw.value
+  createAccount(fn: string, ln: string, e: string, pw: string, cpw: string) {
+    if (pw === cpw && pw.length > 4) {
+      if (this.uService.checkIfUserExists(e)) {
+        alert('An account with this email address already exists. Please log in or try another email address.')
+      } else {
+        let userToBeAdded: User = {
+          firstName: fn,
+          lastName: ln,
+          email: e,
+          password: pw
+        }
+        this.uService.addUser(userToBeAdded);
+      }
 
-  //
+    } else {
+      alert('Your password either is too short or was not confirmed properly. Please try again.')
+    }
+  }
 
 }
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -7,13 +7,24 @@ import { User } from '../interfaces/user';
 export class UserService {
   userArray: User[] = [
     {
-      email: 'sampleemail@sample.sample',
-      password: 'sample'
+      firstName: 'Sample', lastName: 'Sampleton', email: 'sampleemail@sample.sample', password: 'sample'
     }
   ]
   constructor() { }
 
   checkUserInfo(email: string, password: string): boolean {
     return email === this.userArray[0].email ? (password === this.userArray[0].password ? true : false) : false;
+  }
+
+  checkIfUserExists(email: string): boolean {
+    let result = false;
+    if(this.userArray.find( x => email.toLowerCase() === x.email) !== undefined){
+      result = true;
+    };
+    return result;
+  }
+
+  addUser(userToBeAdded: User) {
+    this.userArray.push(userToBeAdded);
   }
 }
